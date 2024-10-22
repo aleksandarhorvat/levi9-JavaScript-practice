0f318f6414ed67fdf86d9deffa26970cb19e8669
Aleksandar Horvat <a.horvat-int@levi9.com>
2024-10-22 14:26:10 +0200

    Adedd GUI implementation of calculator

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 JavaScript practice done during levi9 internship
 
 `calculator.js - simple calculator in console`
+`calculator-gui.js - scipt used by index.js`
+`index.html - simple calculator with gui`
---
+++ calculator-gui.js
@@ -0,0 +1,27 @@
+function calculate() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var operation = document.getElementById("operation").value;
+    var result;
+
+    switch (operation) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            if (num2 !== 0) {
+                result = num1 / num2;
+            } else {
+                result = "You can't divide by 0!";
+            }
+            break;
+    }
+
+    document.getElementById("result").innerText = "The result is: " + (result !== undefined ? result : "");
+}
